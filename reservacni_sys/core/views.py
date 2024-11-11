c71b1ed22f38d8d59316e5247c8ce5a279c32559
martinekmichal <martinek.m.84@gmail.com>
2024-11-11 14:32:38 +0100

    Update views.py

--- reservacni_sys/core/views.py
+++ reservacni_sys/core/views.py
@@ -1,3 +1,41 @@
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.forms import UserChangeForm
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def home_page(request):
+    return render(request, "home.html")
+
+def about_page(request):
+    return render(request, "about.html")
+
+@login_required
+def profile_page(request):
+    return render(request, 'profile.html')
+
+def register_page(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    return render(request,
+                  'register.html',
+                  {'form': UserRegisterForm()})
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profil byl úspěšně aktualizován.")
+            return redirect('profile')
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, 'edit_profile.html', {'form': form})
